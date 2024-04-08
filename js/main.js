53c8fa993fbfb420a893f4872e094b6a13f72a89
burakmrc <burakmeric210@gmail.com>
2024-04-08 13:37:01 +0300

    Galery added

--- js/main.js
+++ js/main.js
@@ -203,7 +203,47 @@
 	};
 	WhatsappFrom();
 
+	$('.portfolio-menu ul li').click(function(){
+		$('.portfolio-menu ul li').removeClass('active');
+		$(this).addClass('active');
+		
+		var selector = $(this).attr('data-filter');
+		$('.portfolio-item').isotope({
+			filter:selector
+		});
+		return  false;
+	});
+	$(document).ready(function() {
+	var popup_btn = $('.popup-btn');
+	popup_btn.magnificPopup({
+	type : 'image',
+	gallery : {
+		enabled : true
+	}
+	});
+	});
 
+	$('.portfolio-menu ul li').click(function(){
+		$('.portfolio-menu ul li').removeClass('active');
+		$(this).addClass('active');
+		
+		var selector = $(this).attr('data-filter');
+		$('.portfolio-item').isotope({
+			filter:selector
+		});
+		return  false;
+	});
+	$(document).ready(function() {
+	var popup_btn = $('.popup-btn');
+	popup_btn.magnificPopup({
+	type : 'image',
+	gallery : {
+		enabled : true
+	}
+	});
+	});
 
 })(jQuery);
+
+
 
